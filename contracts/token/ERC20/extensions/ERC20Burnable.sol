4cdbe09030da26faec98bc0cc252637ad6633a8e
igingu <ionut.gingu@gmail.com>
2023-04-25 18:45:07 +0300

    ERC20Burnable.

--- contracts/token/ERC20/extensions/ERC20Burnable.sol
+++ contracts/token/ERC20/extensions/ERC20Burnable.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.8.0;
 import "../ERC20.sol";
 import "../../../utils/Context.sol";
 
+// @note - Reviewed
 /**
  * @dev Extension of {ERC20} that allows token holders to destroy both their own
  * tokens and those that they have an allowance for, in a way that can be
